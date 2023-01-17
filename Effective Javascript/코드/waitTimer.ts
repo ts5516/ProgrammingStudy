dc47aa772c3497b5d3e05f3ee08c0eea3c50ab4c
ohTaesung <ts5516@naver.com>
2023-01-17 11:54:55 +0900

    web3 코드 추가

--- Effective Javascript/코드/waitTimer.ts
+++ Effective Javascript/코드/waitTimer.ts
@@ -1,25 +1,21 @@
 async function run() {
-    await waitTimer(5);
-    console.log('finished');
-}
-
-async function waitTimer(seconds: number): Promise<void> {
-    try {
-        const result = await promiseTimer(seconds);
-        console.log(result + ' seconds wait');
-    } catch (error) {
-        console.log(error + ': condition value 오류');
-    }
-}
-
-function promiseTimer(seconds: number): Promise<number> {
-    return new Promise((resolve, reject) => {
+    const promiseTimer = (seconds: number) => new Promise((resolve, reject) => {
         if (seconds < 0) {
             reject(seconds);
         } else {
             setTimeout(resolve, seconds * 1000, seconds);
         }
     });
+
+    await wait(promiseTimer(-1));
+
+    //console.log('finished');
+}
+
+async function wait(promiseFunction: Promise<unknown>): Promise<void> {
+    promiseFunction
+        .then(console.log)
+        .catch((error) => console.log("error: " + error));
 }
 
 run();
---
+++ Web3/study_web3.ts
@@ -0,0 +1,67 @@
+import Web3 from "web3";
+
+async function run() {
+    const fromNode = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+    const toNode = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8787"));
+
+    try {
+        const fromAccount = await getAccount(fromNode);
+        const toAccount = await getAccount(toNode);
+
+        await unlockAccount(fromNode, fromAccount, '12345');
+        //await unlockAccount(toNode, toAccount, '12345');
+
+        fromNode.eth.getBalance(fromAccount).then((result) => {
+            console.log("fromNode 보내기 전: " + result);
+        });
+        toNode.eth.getBalance(toAccount).then((result) => {
+            console.log("toNode 보내기 전: " + result);
+        });
+
+        await sendTransaction(fromNode, fromAccount, toAccount, '300000');
+
+        fromNode.eth.getTransactionReceipt
+        fromNode.eth.getBalance(fromAccount).then((result) => {
+            console.log("fromNode 보낸 후: " + result);
+        });
+        toNode.eth.getBalance(toAccount).then((result) => {
+            console.log("toNode 보낸 후: " + result);
+        });
+
+    } catch (error) {
+        console.log('에러 발견\n' + error);
+    }
+}
+
+async function getAccount(web3: Web3): Promise<string> {
+    return web3.eth.getAccounts()
+        .then((result) => { return result[0]; })
+        .catch((error) => { throw new Error(error); })
+}
+
+async function unlockAccount(web3: Web3, account: string, password: string): Promise<void> {
+    return web3.eth.personal.unlockAccount(account, password, 500)
+        .then(() => console.log('Account unlocked: ' + account))
+        .catch((error) => { throw new Error(error); });
+}
+
+async function sendTransaction(fromNode: Web3, fromAccount: string, toAccount: string, value: string): Promise<void> {
+    return fromNode.eth.sendTransaction({
+        from: fromAccount,
+        to: toAccount,
+        gas: 100000,
+        gasPrice: 0,
+        value: Web3.utils.toWei(value, 'wei')
+    })
+        .once('transactionHash', (transactionHash) => {
+            fromNode.eth.getTransaction(transactionHash).then(console.log);
+        })
+        .then((receipt) => {
+            console.log("receipt: " + receipt);
+        })
+        .catch(() => {
+            fromNode.eth.getBalance(fromAccount).then(console.log);
+        });
+}
+
+run();
